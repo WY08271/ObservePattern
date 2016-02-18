2d47e27c217703aa5768c0a271514f44b1743d03
WY08271 <wy08271@outlook.com>
2016-02-19 04:37:45 +0800

    common code for observer pattern

---
+++ src/com/pattern/observer/Observer.java
@@ -0,0 +1,8 @@
+package com.pattern.observer;
+
+/**
+ * 观察者接口,定义一个更新的接口给那些在目标发生改变时被通知的对象
+ */
+public interface Observer {
+    public void update(Subject subject);
+}
--- src/com/pattern/observer/Subject.java
+++ src/com/pattern/observer/Subject.java
@@ -1,4 +1,26 @@
 package com.pattern.observer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  目标对象,它知道观察它的观察者,并提供添加删除观察者的接口
+ */
 public class Subject {
+    private List<Observer> observers = new ArrayList<Observer>();
+
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    protected void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(this);
+        }
+    }
 }
